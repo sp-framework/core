4479c53fc1b60b3a746b26b52f28de69421c6158
Gurdeep Bagga (Guru) <guru@bazaari.com.au>
2020-12-29 00:03:48 +0530

    Fixed Errors component view path.

--- applications/Ecom/Admin/Components/Errors/ErrorsComponent.php
+++ applications/Ecom/Admin/Components/Errors/ErrorsComponent.php
@@ -8,11 +8,15 @@ class ErrorsComponent extends BaseComponent
 {
     public function controllerNotFoundAction()
     {
+        $this->view->setViewsDir($this->modules->views->getPhalconViewPath());
+
         $this->view->pick('common/errors/notfound');
     }
 
     public function actionNotFoundAction()
     {
+        $this->view->setViewsDir($this->modules->views->getPhalconViewPath());
+
         $this->view->pick('common/errors/notfound');
     }
 }
