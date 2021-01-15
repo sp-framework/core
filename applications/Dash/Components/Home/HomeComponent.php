183b3ec4996e67e49727223ab118952749ddae87
Gurdeep Bagga (Guru) <guru@bazaari.com.au>
2021-01-15 12:50:46 +0530

    Home.

--- applications/Dash/Components/Home/HomeComponent.php
+++ applications/Dash/Components/Home/HomeComponent.php
@@ -3,19 +3,22 @@
 namespace Applications\Dash\Components\Home;
 
 use Applications\Dash\Components\Filters\Install\Component as FiltersComponent;
+use Applications\Dash\Components\Hrms\Skills\Install\Component as SkillsComponent;
 use Applications\Dash\Components\Inventory\Brands\Install\Component as BrandsComponent;
 use Applications\Dash\Components\Inventory\Categories\Install\Component as CategoriesComponent;
 use Applications\Dash\Components\Inventory\Specifications\Install\Component as SpecificationsComponent;
 use Applications\Dash\Components\Inventory\Suppliers\Install\Component as SuppliersComponent;
 use Applications\Dash\Components\Storages\Install\Component as StoragesComponent;
-use Applications\Dash\Packages\Hrms\Employees\Install\Package as EmployeesPackage;
-use Applications\Dash\Packages\Channels\Install\Package as ChannelsPackage;
 use Applications\Dash\Packages\ABNLookup\Install\Package as ABNLookupPackage;
+use Applications\Dash\Packages\Businesses\Install\Package as BusinessesPackage;
+use Applications\Dash\Packages\Channels\Install\Package as ChannelsPackage;
+use Applications\Dash\Packages\Hrms\Employees\Install\Package as EmployeesPackage;
+use Applications\Dash\Packages\Hrms\Employees\Settings\Statuses\Install\Package as EmployeesStatusesPackage;
+use Applications\Dash\Packages\Hrms\Skills\Install\Package as SkillsPackage;
 use Applications\Dash\Packages\Inventory\Brands\Install\Package as BrandsPackage;
 use Applications\Dash\Packages\Inventory\Categories\Install\Package as CategoriesPackage;
 use Applications\Dash\Packages\Inventory\Specifications\Install\Package as SpecificationsPackage;
 use Applications\Dash\Packages\Inventory\Suppliers\Install\Package as SuppliersPackage;
-use Applications\Dash\Packages\Businesses\Install\Package as BusinessesPackage;
 use Applications\Dash\Packages\Locations\Install\Package as LocationsPackage;
 use System\Base\BaseComponent;
 
@@ -54,7 +57,6 @@ class HomeComponent extends BaseComponent
         // $channelsPackage = new ChannelsPackage();
         // $channelsPackage->installPackage(true);
 
-
         // For Installing Categories
         // $categoriesComponent = new CategoriesComponent();
         // $categoriesComponent->installComponent();
@@ -69,10 +71,6 @@ class HomeComponent extends BaseComponent
         // $specificationPackage = new SpecificationsPackage();
         // $specificationPackage->installPackage(true);
 
-        // For Installing Employees Types
-        // $employeesTypesPackage = new EmployeesPackage();
-        // $employeesTypesPackage->installPackage(true);
-
         // var_dump($this->basepackages->geoCountries->getById(1));
         // var_dump($this->geodb);
 
@@ -82,20 +80,30 @@ class HomeComponent extends BaseComponent
 
         //
         // For Installing Businesses
-        // $businessesPackage = new BusinessesPackage();
-        // $businessesPackage->installPackage(true);
+        $businessesPackage = new BusinessesPackage();
+        $businessesPackage->installPackage(true);
 
         // For Installing Locations
-        // $locationsPackage = new LocationsPackage();
-        // $locationsPackage->installPackage(true);
+        $locationsPackage = new LocationsPackage();
+        $locationsPackage->installPackage(true);
 
         // For Installing Channels
-        // $channelsPackage = new ChannelsPackage();
-        // $channelsPackage->installPackage(true);
+        $channelsPackage = new ChannelsPackage();
+        $channelsPackage->installPackage(true);
 
-        // For Installing Employees Types
-        // $employeesPackage = new EmployeesPackage();
-        // $employeesPackage->installPackage(true);
+        // For Installing Skills
+        // $skillsComponent = new SkillsComponent();
+        // $skillsComponent->installComponent();
+        // $skillsPackage = new SkillsPackage();
+        // $skillsPackage->installPackage(true);
+
+        // For Installing Employees
+        $employeesPackage = new EmployeesPackage();
+        $employeesPackage->installPackage(true);
+
+        // For Installing Employees Statuses
+        $employeesStatusesPackage = new EmployeesStatusesPackage();
+        $employeesStatusesPackage->installPackage(true);
         // $this->view->disable();
     }
 }
