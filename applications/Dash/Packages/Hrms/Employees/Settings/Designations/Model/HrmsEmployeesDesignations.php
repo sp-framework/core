2e5e5173a23d263acd9e2ed41d39efaa6bdc2109
Gurdeep Bagga (Guru) <guru@bazaari.com.au>
2021-01-21 15:15:10 +0530

    HRMS Designations initial commit

---
+++ applications/Dash/Packages/Hrms/Employees/Settings/Designations/Model/HrmsEmployeesDesignations.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Applications\Dash\Packages\Hrms\Employees\Settings\Designations\Model;
+
+use System\Base\BaseModel;
+
+class HrmsEmployeesDesignations extends BaseModel
+{
+    public $id;
+
+    public $name;
+
+    public $description;
+
+    public $employees_count;
+}
