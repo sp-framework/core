22d2a7d89943347ac5a006f6e1677e184b7fe865
Gurdeep Bagga (Guru) <guru@bazaari.com.au>
2021-02-21 00:47:33 +0530

    Moved external composer files to Internal loader.

--- system/Base/Loader/Files.php
+++ system/Base/Loader/Files.php
@@ -1,6 +1,11 @@
 <?php
 
 return
-	[
-		'../system/Base/Helpers.php'
-	];
+    [
+        '../system/Base/Helpers.php',
+        '../system/Base/Providers/ContentServiceProvider/Remote/vendor/guzzlehttp/guzzle/src/functions_include.php',
+        '../system/Base/Providers/ContentServiceProvider/Remote/vendor/guzzlehttp/promises/src/functions_include.php',
+        '../system/Base/Providers/ContentServiceProvider/Remote/vendor/guzzlehttp/psr7/src/functions_include.php',
+        '../system/Base/Providers/ContentServiceProvider/Remote/vendor/ralouphie/getallheaders/src/getallheaders.php'
+    ];
+
--- system/Base/Loader/Namespaces.php
+++ system/Base/Loader/Namespaces.php
@@ -2,6 +2,14 @@
 
 return
     [
-        'Apps'        => '../apps/',
-        'System'      => '../system/'
+        'Apps'                          => '../apps/',
+        'System'                        => '../system/',
+        'GuzzleHttp'                    => '../system/Base/Providers/ContentServiceProvider/Remote/vendor/guzzlehttp/guzzle/src/',
+        'GuzzleHttp\\Promise'           => '../system/Base/Providers/ContentServiceProvider/Remote/vendor/guzzlehttp/promises/src/',
+        'GuzzleHttp\\Psr7'              => '../system/Base/Providers/ContentServiceProvider/Remote/vendor/guzzlehttp/psr7/src/',
+        'Psr\\Http\\Client'             => '../system/Base/Providers/ContentServiceProvider/Remote/vendor/psr/http-client/src/',
+        'Psr\\Http\\Message'            => '../system/Base/Providers/ContentServiceProvider/Remote/vendor/psr/http-message/src/',
+        'League\\Flysystem'             => '../system/Base/Providers/ContentServiceProvider/Local/vendor/league/flysystem/src/',
+        'League\\MimeTypeDetection'     => '../system/Base/Providers/ContentServiceProvider/Local/vendor/league/mime-type-detection/src',
+        'PHPMailer\\PHPMailer'          => '../system/Base/Providers/BasepackagesServiceProvider/Packages/Email/vendor/phpmailer/phpmailer/src/'
     ];
