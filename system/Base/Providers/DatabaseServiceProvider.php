ef031bb433a9fac98e14354763fc404b41c70d98
Gurdeep Bagga (Guru) <guru@bazaari.com.au>
2021-07-24 12:39:26 +1000

    #46 Added new PDO for CLI as we need session for http pdo.

--- system/Base/Providers/DatabaseServiceProvider.php
+++ system/Base/Providers/DatabaseServiceProvider.php
@@ -6,16 +6,38 @@ use Phalcon\Di\DiInterface;
 use Phalcon\Di\ServiceProviderInterface;
 use System\Base\Providers\DatabaseServiceProvider\ModelsManager;
 use System\Base\Providers\DatabaseServiceProvider\Pdo;
+use System\Base\Providers\DatabaseServiceProvider\PdoCli;
 
 class DatabaseServiceProvider implements ServiceProviderInterface
 {
 	public function register(DiInterface $container) : void
 	{
+		if (PHP_SAPI === 'cli') {
+
+			$container->setShared(
+				'db',
+				function () use ($container) {
+					$dbConfig = $container->getShared('config')->db;
+					return (new PdoCli($dbConfig))->init();
+				}
+			);
+
+			$container->setShared(
+				'modelsManager',
+				function () {
+					return (new ModelsManager())->init();
+				}
+			);
+
+			return;
+		}
+
 		$container->setShared(
 			'db',
 			function () use ($container) {
 				$dbConfig = $container->getShared('config')->db;
-				return (new Pdo($dbConfig))->init();
+				$session = $container->getShared('session');
+				return (new Pdo($dbConfig, $session))->init();
 			}
 		);
 
