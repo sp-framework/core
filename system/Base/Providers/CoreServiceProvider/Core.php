a86cd1afb26efa065ce07e9c9922d9fa27fbcb79
Gurdeep Bagga (Guru) <guru@bazaari.com.au>
2022-01-13 10:04:16 +1100

    !212

--- system/Base/Providers/CoreServiceProvider/Core.php
+++ system/Base/Providers/CoreServiceProvider/Core.php
@@ -2,6 +2,9 @@
 
 namespace System\Base\Providers\CoreServiceProvider;
 
+use League\Flysystem\FilesystemException;
+use League\Flysystem\UnableToReadFile;
+use League\Flysystem\UnableToRetrieveMetadata;
 use Phalcon\Helper\Json;
 use System\Base\BasePackage;
 use System\Base\Providers\CoreServiceProvider\Model\Core as CoreModel;
@@ -20,22 +23,53 @@ class Core extends BasePackage
 
 		$this->core['settings'] = Json::decode($this->core['settings'], true);
 
-		if (isset($this->core['settings']['sigKey']) &&
-			isset($this->core['settings']['sigText']) &&
-			isset($this->core['settings']['cookiesSig'])
-		) {
-			$sigKey = $this->core['settings']['sigKey'];
-			$sigText = $this->core['settings']['sigText'];
-			$cookiesSig = $this->core['settings']['cookiesSig'];
-		} else {
-			$this->core['settings']['sigKey'] = $sigKey = $this->random->base58();
-			$this->core['settings']['sigText'] = $sigText = $this->random->base58(32);
-			$this->core['settings']['cookiesSig'] = $cookiesSig = $this->crypt->encryptBase64($sigText, $sigKey);
-			$this->core['settings'] = Json::encode($this->core['settings']);
-
-			$this->update($this->core);
-		}
+		$this->checkKeys();
 
 		return $this;
+	}
+
+	protected function checkKeys()
+	{
+		try {
+			$fileExists = $this->localContent->fileExists('system/.keys');
+		} catch (FilesystemException | UnableToRetrieveMetadata $exception) {
+			throw $exception;
+		}
+
+		if (!$fileExists) {
+			$this->createKeys();
+
+			$this->getKeys();
+		} else {
+			$this->getKeys();
+		}
+	}
+
+	protected function createKeys()
+	{
+		$keys['sigKey'] = $sigKey = $this->random->base58();
+		$keys['sigText'] = $sigText = $this->random->base58(32);
+		$keys['cookiesSig'] = $cookiesSig = $this->crypt->encryptBase64($sigText, $sigKey);
+
+		try {
+			$this->localContent->write('system/.keys', Json::encode($keys));
+		} catch (FilesystemException | UnableToWriteFile $exception) {
+			throw $exception;
+		}
+	}
+
+	protected function getKeys()
+	{
+		try {
+			$keysFile = $this->localContent->read('system/.keys');
+		} catch (FilesystemException | UnableToReadFile | \Exception $exception) {
+			throw $exception;
+		}
+
+		$keys = Json::decode($keysFile, true);
+
+		$this->core['settings']['sigKey'] = $keys['sigKey'];
+		$this->core['settings']['sigText'] = $keys['sigText'];
+		$this->core['settings']['cookiesSig'] = $keys['cookiesSig'];
 	}
 }
