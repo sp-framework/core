76cebcaec949af6d7e202d8c8fcb91f5eb5138fa
Gurdeep Bagga (Guru) <guru@bazaari.com.au>
2022-10-19 08:04:51 +0530

    !228 Remove APIs

--- apps/Dash/Packages/System/Api/Apis/Ebay/EbayTradingApi/Enums/WarningLevelCodeType.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Apps\Dash\Packages\System\Api\Apis\Ebay\EbayTradingApi\Enums;
-
-class WarningLevelCodeType
-{
-    const C_HIGH = 'High';
-    const C_LOW = 'Low';
-}
--- apps/Dash/Packages/System/Api/Apis/Ebay/EbayTradingApi/Enums/WarrantyOfferedCodeType.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Apps\Dash\Packages\System\Api\Apis\Ebay\EbayTradingApi\Enums;
-
-class WarrantyOfferedCodeType
-{
-    const C_CUSTOM_CODE = 'CustomCode';
-    const C_WARRANTY_OFFERED = 'WarrantyOffered';
-}
