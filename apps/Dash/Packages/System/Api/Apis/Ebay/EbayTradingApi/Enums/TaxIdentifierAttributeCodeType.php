ad254326ac522e5afd4acf32328999ac8beda40e
Gurdeep Bagga (Guru) <guru@bazaari.com.au>
2022-10-19 08:04:51 +0530

    !223 Remove APIs

--- apps/Dash/Packages/System/Api/Apis/Ebay/EbayTradingApi/Enums/TaxIdentifierAttributeCodeType.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Apps\Dash\Packages\System\Api\Apis\Ebay\EbayTradingApi\Enums;
-
-class TaxIdentifierAttributeCodeType
-{
-    const C_CUSTOM_CODE = 'CustomCode';
-    const C_ISSUING_COUNTRY = 'IssuingCountry';
-}
