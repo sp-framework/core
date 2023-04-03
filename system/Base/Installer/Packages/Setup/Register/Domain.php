25305532934b9fd9a618a51122ab369fdd4f21b6
Gurdeep Bagga (Guru) <guru@bazaari.com.au>
2023-04-03 18:22:20 +1000

    !292 - Forgot to add setup domain registration changes.

--- system/Base/Installer/Packages/Setup/Register/Domain.php
+++ system/Base/Installer/Packages/Setup/Register/Domain.php
@@ -2,6 +2,9 @@
 
 namespace System\Base\Installer\Packages\Setup\Register;
 
+use BlueLibraries\Dns\DnsRecords;
+use BlueLibraries\Dns\Handlers\Types\TCP;
+use BlueLibraries\Dns\Records\RecordTypes;
 use Phalcon\Helper\Json;
 
 class Domain
@@ -50,18 +53,98 @@ class Domain
 	protected function validateDomain($domain)
 	{
 		$record = [];
+		$record['internal'] = false;
 
 		try {
-			$record['AAAA'] = \dns_get_record($domain, DNS_AAAA);
-			$record['A'] = \dns_get_record($domain, DNS_A);
-			$record['CNAME'] = \dns_get_record($domain, DNS_CNAME);
+			$dnsHandler = (new TCP())
+				->setPort(53)
+				->setNameserver('8.8.8.8')
+				->setTimeout(3) // limit execution to 3 seconds
+				->setRetries(3); // allows 5 retries if response fails
+
+			$dnsRecordsService = new DnsRecords($dnsHandler);
+
+			$record['AAAA'] = $dnsRecordsService->get($domain, RecordTypes::AAAA);
+			$aaaa = [];
+			if (count($record['AAAA']) > 0) {
+				if (count($record['AAAA']) === 1) {
+					$aaaaRecord = $record['AAAA'][0]->toArray();
+					array_push($aaaa, $aaaaRecord['ipv6']);
+				} else {
+					foreach ($record['AAAA'] as $aaaaRecord) {
+						$aaaaRecord = $aaaaRecord->toArray();
+
+						if (isset($aaaaRecord['ipv6'])) {
+							array_push($aaaa, $aaaaRecord['ipv6']);
+						}
+					}
+				}
+			}
+			$record['AAAA'] = $aaaa;
+
+			$record['A'] = $dnsRecordsService->get($domain, RecordTypes::A);
+			$a = [];
+			if (count($record['A']) > 0) {
+				if (count($record['A']) === 1) {
+					$aRecord = $record['A'][0]->toArray();
+					array_push($a, $aRecord['ip']);
+				} else {
+					foreach ($record['A'] as $aRecord) {
+						$aRecord = $aRecord->toArray();
+
+						if (isset($aRecord['ip'])) {
+							array_push($a, $aRecord['ip']);
+						}
+					}
+				}
+			}
+			$record['A'] = $a;
+			$record['CNAME'] = $dnsRecordsService->get($domain, RecordTypes::CNAME);
+			if (count($record['CNAME']) > 0) {
+				$record['CNAME'] = $record['CNAME'][0]->toArray();
+			}
+
+			$record['SOA'] = $dnsRecordsService->get($domain, RecordTypes::SOA);
+			if (count($record['SOA']) > 0) {
+				$record['SOA'] = $record['SOA'][0]->toArray();
+			}
 
 			if (count($record['AAAA']) === 0 &&
 				count($record['A']) === 0 &&
 				count($record['CNAME']) === 0
 			) {
-				return false;
+				$record['internal'] = true;
 			}
+
+			$keys = [
+				'HTTP_CLIENT_IP',
+				'HTTP_X_FORWARDED_FOR',
+				'HTTP_X_FORWARDED',
+				'HTTP_X_CLUSTER_CLIENT_IP',
+				'HTTP_FORWARDED_FOR',
+				'HTTP_FORWARDED',
+				'REMOTE_ADDR'
+			];
+
+			$record['server_address'] = $_SERVER['REMOTE_ADDR'];
+
+			foreach ($keys as $key) {
+				if (array_key_exists($key, $_SERVER) === true) {
+					foreach (array_map('trim', explode(',', $_SERVER[$key])) as $ip) {
+						if ($record['internal']) {
+							$flags = FILTER_FLAG_NO_RES_RANGE;
+						} else {
+							$flags = FILTER_FLAG_NO_PRIV_RANGE | FILTER_FLAG_NO_RES_RANGE;
+						}
+
+						if (filter_var($ip, FILTER_VALIDATE_IP, $flags) !== false) {
+							$record['server_address'] = $ip;
+						}
+					}
+				}
+			}
+
+			$this->packagesData->domainDetails = $record;
 
 			return $record;
 		} catch (\Exception $e) {
