1e198001e472fc1642c6bd1b39b6e089b896959c
Gurdeep Bagga (Guru) <guru@bazaari.com.au>
2023-04-05 01:48:06 +1000

    !325 - Changing Base config file to just have setup set to true. This will be overwritten during setup, so this state would be the default state of initial install.

--- system/Configs/Base.php
+++ system/Configs/Base.php
@@ -2,42 +2,5 @@
 
 return
 	[
-		"setup" 			=> false,
-		"dev"    			=> false, //true - Development false - Production
-		"debug"				=> false,
-		"db" 				=>
-		[
-			"host" 							=> "localhost",
-			"dbname" 						=> "sp",
-			"username" 						=> "sp",
-			"password" 						=> "123",
-			"port" 							=> "3306",
-			"charset" 	 	    			=> "utf8mb4"
-		],
-		"cache"				=>
-		[
-			"enabled"						=> true, //Global Cache value //true - Production false - Development
-			"timeout"						=> 60, //Global Cache timeout in seconds
-			"service"						=> "streamCache"
-		],
-		"security"			=>
-		[
-			"passwordWorkFactor"			=> 8,
-			"cookiesWorkFactor" 			=> 4,
-		],
-		"logs"				=>
-		[
-			"enabled"						=> true,
-			"exceptions"					=> true,
-			"level"							=> "INFO",
-			"service"						=> "streamLogs",
-			"emergencyEmailLogs"			=> false,
-			"emergencyLogsEmailAddresses"	=> "",
-		],
-		"websocket"			=>
-		[
-			"protocol"						=> "tcp",
-			"host"							=> "localhost",
-			"port"							=> 5555
-		]
+		"setup" 		=> true
 	];
