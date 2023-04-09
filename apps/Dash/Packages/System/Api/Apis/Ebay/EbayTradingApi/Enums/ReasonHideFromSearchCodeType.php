486f3a9c31c1754d574f9ff153eb45f658ff509a
Gurdeep Bagga (Guru) <guru@bazaari.com.au>
2023-04-09 22:13:15 +1000

    !228 - Remove ebay Api files.

--- apps/Dash/Packages/System/Api/Apis/Ebay/EbayTradingApi/Enums/ReasonHideFromSearchCodeType.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Apps\Dash\Packages\System\Api\Apis\Ebay\EbayTradingApi\Enums;
-
-class ReasonHideFromSearchCodeType
-{
-    const C_DUPLICATE_LISTING = 'DuplicateListing';
-    const C_OUT_OF_STOCK = 'OutOfStock';
-}
--- apps/Dash/Packages/System/Api/Apis/Ebay/EbayTradingApi/Enums/ShippingServiceType.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Apps\Dash\Packages\System\Api\Apis\Ebay\EbayTradingApi\Enums;
-
-class ShippingServiceType
-{
-    const C_DOMESTIC = 'Domestic';
-    const C_INTERNATIONAL = 'International';
-}
