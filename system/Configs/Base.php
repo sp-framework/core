a5051041e3f5abd82078d28d4cf89a89392ad8af
Gurdeep Bagga (Guru) <guru@bazaari.com.au>
2023-04-26 14:51:26 +1000

    !399 - Update base config

--- system/Configs/Base.php
+++ system/Configs/Base.php
@@ -3,6 +3,12 @@
 return
 	[
 		"setup" 			=> true,
+		"cache"				=>
+		[
+			"enabled"						=> false, //Global Cache value //true - Production false - Development
+			"timeout"						=> 60, //Global Cache timeout in seconds
+			"service"						=> "streamCache"
+		],
 		"logs"				=>
 		[
 			"enabled"						=> "false",
