f09cf1271e94d7844164c18c86770f6ee8080168
Gurdeep Bagga (Guru) <guru@bazaari.com.au>
2024-03-12 16:04:49 +1100

    !390 - Base config after reset.

--- system/Configs/Base.php
+++ system/Configs/Base.php
@@ -2,43 +2,20 @@
 
 return
 	[
-		"setup" 			=> false,
-		"dev"    			=> true, //true - Development false - Production
-		"debug"				=> true,
-		"auto_off_debug"	=> 0,
-		"databasetype" 		=> "hybrid",
-		"db" 				=>
-		[
-			"host" 							=> "localhost",
-			"port" 							=> "3306",
-			"dbname" 						=> "sp",
-			"charset" 	 	    			=> "utf8mb4",
-			"collation" 	    			=> "utf8mb4_general_ci",
-			"username" 						=> "sp",
-			"password" 						=> "r2czMyUAuNPDN3FSfdM7zT+i+LnywKIZzqewh9E9nyCOn1o7gNnAfralTC3jlqou1fqU"
-		],
-		"ff" 				=>
-		[
-			"databaseDir" 					=> "sp/"
-		],
+		"setup" 			=> true,
 		"cache"				=>
 		[
 			"enabled"						=> false, //Global Cache value //true - Production false - Development
 			"timeout"						=> 60, //Global Cache timeout in seconds
 			"service"						=> "streamCache"
 		],
-		"security"			=>
-		[
-			"passwordWorkFactor"			=> 2,
-			"cookiesWorkFactor" 			=> 2,
-		],
 		"logs"				=>
 		[
-			"enabled"						=> "true",
+			"enabled"						=> "false",
 			"exceptions"					=> "false",
 			"level"							=> "DEBUG",
 			"service"						=> "streamLogs",
-			"emergencyLogsEmail"			=> "true",
+			"emergencyLogsEmail"			=> "false",
 			"emergencyLogsEmailAddresses"	=> "",
 		],
 		"websocket"			=>
@@ -46,5 +23,9 @@ return
 			"protocol"						=> "tcp",
 			"host"							=> "localhost",
 			"port"							=> 5555
+		],
+		"timeout"			=>
+		[
+			"cookies"						=> 86400
 		]
 	];
