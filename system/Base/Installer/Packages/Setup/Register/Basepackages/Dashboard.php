eba4460a2fe4b6d77512c758a3389fa594994dee
Gurdeep Singh (Guru) <oyeaussie@gmail.com>
2025-02-05 14:00:57 +1100

    Fix issue #591

--- system/Base/Installer/Packages/Setup/Register/Basepackages/Dashboard.php
+++ system/Base/Installer/Packages/Setup/Register/Basepackages/Dashboard.php
@@ -8,7 +8,7 @@ class Dashboard
     {
         $dashboard =
             [
-                'name'                  => 'Default',
+                'name'                  => 'Core Default',
                 'app_id'                => 1,
                 'app_default'           => 1,
                 'created_by'            => 1,
@@ -23,6 +23,35 @@ class Dashboard
             $dashboardStore = $ff->store('basepackages_dashboards');
 
             $dashboardStore->updateOrInsert($dashboard);
+        }
+
+        //Register Timezone Widget
+        $widget =
+            [
+                "dashboard_id"              =>  1,
+                "sequence"                  =>  0,
+                "widget_id"                 =>  1,
+                "settings"                  =>
+                    $helper->encode([
+                        "minW"              =>  "3",
+                        "method"            =>  "worldClock",
+                        "widget_id"         =>  "1",
+                        "dashboard_id"      =>  "1",
+                        "x"                 =>  "0",
+                        "y"                 =>  "0",
+                        "clocks"            => ["australiamelbourne"],
+                        "w"                 =>  "12"
+                    ])
+            ];
+
+        if ($db) {
+            $db->insertAsDict('basepackages_dashboards_widgets', $widget);
+        }
+
+        if ($ff) {
+            $widgetStore = $ff->store('basepackages_dashboards_widgets');
+
+            $widgetStore->updateOrInsert($widget);
         }
     }
 }
