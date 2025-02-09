891bca01c6fb49a666d1d2d866cfb2e288ab8108
Gurdeep Singh (Guru) <oyeaussie@gmail.com>
2025-02-09 23:25:54 +1100

    update issue #34. fix menu installer.

--- system/Base/Providers/ModulesServiceProvider/MenuInstaller.php
+++ system/Base/Providers/ModulesServiceProvider/MenuInstaller.php
@@ -2,15 +2,40 @@
 
 namespace System\Base\Providers\ModulesServiceProvider;
 
+use League\Flysystem\FilesystemException;
+use League\Flysystem\UnableToCheckExistence;
+use League\Flysystem\UnableToReadFile;
 use System\Base\BasePackage;
 
 class MenuInstaller extends BasePackage
 {
-    public function installMenu(array $componentJsonFileArr)
+    public function installMenu($componentClass)
     {
-        $menu = $componentJsonFileArr['menu'];
+        try {
+            $file = 'apps/' . implode('/', array_slice(explode('\\', get_class($componentClass)), 1, -1)) . '/component.json';
 
-        trace([$menu]);
+            if ($this->localContent->fileExists($file)) {
+                $installComponentJsonFile = $this->helper->decode($this->localContent->read($file), true);
+
+                if (!isset($installComponentJsonFile['menu']) ||
+                    (isset($installComponentJsonFile['menu']) && (bool) $installComponentJsonFile['menu'] === false)
+                ) {
+                    return true;
+                }
+
+                $menu = $this->basepackages->menus->getMenusByRouteForAppType($installComponentJsonFile['route'], $installComponentJsonFile['app_type']);
+
+                if ($menu) {
+                    $this->basepackages->menus->updateMenu($menu['id'], $installComponentJsonFile);
+                } else {
+                    $this->basepackages->menus->addMenu($installComponentJsonFile);
+                }
+            }
+        } catch (FilesystemException | UnableToCheckExistence | UnableToReadFile | \throwable $e) {
+            trace([$e]);
+            throw $e;
+        }
+
         return true;
     }
 }
