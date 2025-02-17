0f6e313efd0c038499d2df4889ff23a6b1b78e60
Gurdeep Singh (Guru) <oyeaussie@gmail.com>
2025-02-17 15:45:23 +1100

    fix issue #613

--- apps/Core/Packages/Devtools/Modules/DevtoolsModules.php
+++ apps/Core/Packages/Devtools/Modules/DevtoolsModules.php
@@ -952,7 +952,11 @@ class DevtoolsModules extends BasePackage
 
                 $routePath = implode('/', $pathArr) . '/Install/';
             } else if ($data['module_type'] === 'views') {
-                if ($data['base_view_module_id'] == 0) {
+                if ($data['is_subview'] == true &&
+                    $data['base_view_module_id'] == 0
+                ) {
+                    throw new \Exception('View dependency for this subview not set.');
+                } else if ($data['base_view_module_id'] == 0) {
                     $routePath = $data['name'] . '/';
                 } else {
                     $baseView = $this->modules->views->getViewById($data['base_view_module_id']);
@@ -3051,6 +3055,12 @@ $file .= '
 
             if ($data['module_type'] === 'views' && isset($data['is_subview']) && $data['is_subview'] == 'true') {
                 $baseView = $this->modules->views->getViewById($data['base_view_module_id']);
+
+                if (!$baseView) {
+                    $this->addResponse('View dependency for this subview not set.', 1);
+
+                    return false;
+                }
             }
 
             if ($data['module_type'] === 'components' || $data['module_type'] === 'apps_types') {
