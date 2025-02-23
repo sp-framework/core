3d8be48d7ec6a9a350b6574d999cfaf48dc2f6e9
Gurdeep Singh (Guru) <oyeaussie@gmail.com>
2025-02-23 23:03:45 +1100

    fix issue #630

--- system/Base/Providers/DatabaseServiceProvider/Ff/Store.php
+++ system/Base/Providers/DatabaseServiceProvider/Ff/Store.php
@@ -133,6 +133,7 @@ class Store
 
                             if (count($relation) > 0 && isset($relation[2])) {
                                 try {
+                                    $relation[2] = explode('+', $relation[2])[0];
                                     $relationsStore = new Store($relation[2], $this->databasePath, $this->ff);
 
                                     $rmd = array_replace_recursive($rmd, $relationsStore->getSchemaMetaData());
