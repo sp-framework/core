dc067caba8d2e7b21309442f42caf5ad14d882c8
Gurdeep Singh (Guru) <oyeaussie@gmail.com>
2025-06-15 15:39:02 +1000

    update issue #702

--- system/Base/Installer/Packages/Setup/Schema/Basepackages/Geo/Regions.php
+++ system/Base/Installer/Packages/Setup/Schema/Basepackages/Geo/Regions.php
@@ -1,11 +1,11 @@
 <?php
 
-namespace System\Base\Installer\Packages\Setup\Schema\Basepackages\Geo\Regions;
+namespace System\Base\Installer\Packages\Setup\Schema\Basepackages\Geo;
 
 use Phalcon\Db\Column;
 use Phalcon\Db\Index;
 
-class GeoRegions
+class Regions
 {
     public function columns()
     {
