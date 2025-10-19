ebeb53939896355c7ce0cc3ec1f5642273bdfc5b
Gurdeep Singh (Guru) <oyeaussie@gmail.com>
2025-10-19 09:42:30 +0530

    update issue #744. Disable ping and tunnels when user is not logged in.

--- public/core/default/js/header/dependencies/Baz/BazCore.js
+++ public/core/default/js/header/dependencies/Baz/BazCore.js
@@ -88,17 +88,17 @@ var BazCore = function() {
                         cache: true
                     }).done(function() {
                         $('body').trigger('libsLoadComplete');
-                        bazFooterFunctions(_extends(BazCore.defaults, options));
+                        bazFooterFunctions(options);
                         dataCollection.env.libsLoaded = true;
                     });
                 });
             }
         } else if (dataCollection.env.libsLoaded === true) {
-            bazFooterFunctions(_extends(BazCore.defaults, options));
+            bazFooterFunctions(options);
         }
     }
     //Footer
-    function bazFooterFunctions() {
+    function bazFooterFunctions(options) {
         PNotify.defaultModules.set(PNotifyBootstrap4, {});
         PNotify.defaultModules.set(PNotifyFontAwesome5, {});
         PNotify.defaultModules.set(PNotifyFontAwesome5Fix, {});
@@ -113,19 +113,19 @@ var BazCore = function() {
             openMenu();
         }
 
-        if (dataCollection.env.currentRoute.indexOf('auth') === -1) {
-            BazTunnels.init();
-        }
-        $('#body').on('bazContentLoaderAjaxComplete', function() {
-            //eslint-disable-next-line
-            console.log(dataCollection.env.wsTunnels.pusher._websocket_connected);
-            if (dataCollection.env.wsTunnels.pusher._websocket_connected !== 'undefined' &&
-                dataCollection.env.wsTunnels.pusher._websocket_connected === false
-            ) {
+        if (!options.guest) {
+            if (dataCollection.env.currentRoute.indexOf('auth') === -1) {
                 BazTunnels.init();
             }
-        });
-        initPings();
+            $('#body').on('bazContentLoaderAjaxComplete', function() {
+                if (dataCollection.env.wsTunnels.pusher._websocket_connected !== 'undefined' &&
+                    dataCollection.env.wsTunnels.pusher._websocket_connected === false
+                ) {
+                    BazTunnels.init();
+                }
+            });
+            initPings();
+        }
     }
 
     //10 mins get update of site status. Note this is not PING, but webserver responsive time to reply with favicon.
