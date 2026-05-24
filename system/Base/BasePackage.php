781f32185a412f513f332b0610072484591789a5
Gurdeep Singh (Guru) <oyeaussie@gmail.com>
2026-05-24 07:48:36 +0530

    update issue #775

--- system/Base/BasePackage.php
+++ system/Base/BasePackage.php
@@ -595,7 +595,7 @@ abstract class BasePackage extends Controller
 
 		//Retrieve from Users Env
 		if ($this->access->auth->check()) {
-			if (count($this->postData()) === 0) {
+			if (count($this->postData()) === 0 || !isset($this->postData()['conditions'])) {
 				$envParams = $this->basepackages->accounts->checkUpdateEnv($this->access->auth->account()['id'], [], false, true);
 
 				if ($envParams) {
