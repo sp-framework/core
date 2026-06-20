c1f4f1d696203bfd6dc51fc192779f88c3599d4d
Gurdeep Singh (Guru) <oyeaussie@gmail.com>
2026-06-20 20:23:59 +0530

    fix issue #832

--- system/Base/Providers/WebSocketServiceProvider/Wss.php
+++ system/Base/Providers/WebSocketServiceProvider/Wss.php
@@ -16,11 +16,15 @@ class Wss
 
     protected $helper;
 
-    public function __construct($config, $helper)
+    protected $logger;
+
+    public function __construct($config, $helper, $logger)
     {
         $this->config = $config;
 
         $this->helper = $helper;
+
+        $this->logger = $logger;
     }
 
     public function init()
@@ -36,11 +40,19 @@ class Wss
             return;
         }
 
-        $this->connector = 
-            $this->config->websocket->protocol . 
-            '://' . 
-            $this->config->websocket->host . 
-            ':' . 
+        if ($this->config->websocket->port == '0' || $this->config->websocket->port === '') {
+            $this->logger->log->debug(
+                'Websocket is configured to connect on port ' .
+                $this->config->websocket->port .
+                '. Standard port is 5555, please verify ZMQ and core settings.'
+            );
+        }
+
+        $this->connector =
+            $this->config->websocket->protocol .
+            '://' .
+            $this->config->websocket->host .
+            ':' .
             $this->config->websocket->port;
     }
 
@@ -59,7 +71,7 @@ class Wss
         return $this->context;
     }
 
-    public function setSocket($contextName = 'New Notification')
+    public function setSocket($contextName = 'SPWss')
     {
         if (!$this->context) {
             $this->setContext();
@@ -73,7 +85,7 @@ class Wss
         return $this->socket;
     }
 
-    public function send(array $data, $contextName = 'New Notification')
+    public function send(array $data, $contextName = 'SPWss')
     {
         if (!$this->connector) {
             $this->setConnector();
